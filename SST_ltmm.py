974d8ed37ecb61d842d00d76fb46713eb3617d5f
alclan2 <allclancy20@gmail.com>
2026-03-04 21:20:17 -0800

    saved SST_ltmm 1deg and 4deg netcdf files; filtered sst_ltmm on sub-basins and saved to net cdf file

--- SST_ltmm.py
+++ SST_ltmm.py
@@ -9,6 +9,7 @@ from shapely.geometry import Polygon, MultiPolygon, Point
 from shapely.ops import transform
 import cartopy.feature as cfeature
 import numpy as np
+import regionmask
 
 # read in basin definition file
 polygons_dict = {}
@@ -96,11 +97,95 @@ sst_4deg = sst_filt.coarsen(
     boundary="trim"
 ).mean()
 
-# checks
-print(sst_filt.shape)
+# save filtered datasets
+sst_filt.to_netcdf("SST_ltmm_NAtl_1deg.nc")
+sst_4deg.to_netcdf("SST_ltmm_NAtl_4deg.nc")
 
-print(float(sst_filt.min()), float(sst_filt.max()))
+# add sub basin region mask
+sst_subbasin = xr.open_dataset("SST_ltmm_NAtl_1deg.nc", decode_times=time_coder)
 
-sst_4deg.isel(time=0).plot(cmap="viridis")
-plt.title("Basin SST 4° Grid (Month 1)")
-plt.show()
+# read in tc_subbasins_NAtl file
+sub_polygons_dict = {}
+
+with open("tc_subbasins_NAtl.dat", "r") as f:
+    for line in f:
+        line = line.strip()
+        if not line or line.startswith("#"):
+            continue
+
+        parts = line.split(",")
+        sub_basin_name = parts[0].replace('"', '')
+        n_vertices = int(parts[1])
+
+        lon_vals = list(map(float, parts[2:2+n_vertices]))
+        lat_vals = list(map(float, parts[2+n_vertices:2+2*n_vertices]))
+
+        coords = list(zip(lon_vals, lat_vals))
+        poly = Polygon(coords)
+
+        if sub_basin_name not in sub_polygons_dict:
+            sub_polygons_dict[sub_basin_name] = []
+        sub_polygons_dict[sub_basin_name].append(poly)
+
+# Convert to GeoDataFrame
+sub_basin_records = []
+
+for name, poly_list in sub_polygons_dict.items():
+    if len(poly_list) == 1:
+        geom = poly_list[0]
+    else:
+        geom = MultiPolygon(poly_list)
+
+    sub_basin_records.append({
+        "sub_basin_name": name,
+        "geometry": geom
+    })
+
+sub_basins = gpd.GeoDataFrame(sub_basin_records, crs="EPSG:4326",geometry="geometry")
+
+# fix invalid polygons
+sub_basins["geometry"] = sub_basins["geometry"].buffer(0)
+
+# remove empty geometries
+sub_basins = sub_basins[~sub_basins.geometry.is_empty]
+
+# longitude conversion
+import shapely.ops
+def shift_lon(geom):
+    return shapely.ops.transform(
+        lambda x, y: (((x + 180) % 360) - 180, y),
+        geom
+    )
+
+# shift lon
+sub_basins["geometry"] = sub_basins["geometry"].apply(shift_lon)
+
+# create a region mask
+regions = regionmask.from_geopandas(
+    sub_basins,
+    names="sub_basin_name"
+)
+
+# create the polygon mask
+mask = regions.mask(sst_subbasin)
+
+# add mask to dataset
+sst_subbasin["sub_basin_id"] = mask
+
+# save sub basin names
+sst_subbasin["sub_basin_id"].attrs["sub_basin_names"] = list(sub_basins["sub_basin_name"])
+
+# remove problematic metadata
+var = sst_subbasin["sst"]
+var.attrs.pop("missing_value", None)
+var.attrs.pop("_FillValue", None)
+
+# clear encoding that may still contain missing_value
+var.encoding = {}
+
+# save sst dataset with sub basin ids
+sst_subbasin.to_netcdf("SST_ltmm_NAtl_subbasins.nc")
+
+
+for i, name in enumerate(sub_basins["sub_basin_name"]):
+    print(i, name)
