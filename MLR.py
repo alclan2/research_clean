c23dd449d2c41857cff88b4116b3f59e160c117d
alclan2 <allclancy20@gmail.com>
2026-08-06 13:03:07 -0700

    MLR runs with TC+TD origins for gpi+sst_anom and gpi+sst_mean

--- MLR.py
+++ MLR.py
@@ -136,8 +136,7 @@ merged = merged[(merged["year"] >= 1940) & (merged["year"] <= 2025)]
 ########################################################################################################################
 
 # standardize variables for MLR
-
-predictors = ["rh600"]
+predictors = ['gpi', 'sst_anom']
 
 # save MLR results
 results = {}
@@ -166,7 +165,7 @@ for basin, group in merged.groupby("sub_basin_name"):
     group[predictors] = scaler.fit_transform(group[predictors])
 
     model = smf.ols(
-        "origin_node_count ~ rh600",
+        "origin_node_count ~ gpi + sst_anom",
         data=group
     ).fit()
 
@@ -205,98 +204,160 @@ for basin, model in results.items():
 
 coef_df = pd.DataFrame(coef_results)
 
-# print(coef_df)
+print(coef_df)
 
-# # save coef table as csv
-# coef_df.to_csv("datasets/data_viz/MLR/TC+TD/single_var_coef_tables/MLR_origin_nodes_vs_gpi_coef_table.csv")
+# save coef table as csv
+# coef_df.to_csv("datasets/data_viz/MLR/TC+TD/gpi+sst_mean_coef_tables/MLR_origin_nodes_gpi+sstMean_coef_table.csv")
 
 ######################################################################################################
 
+# # # actual v predicted for TWO variables
 # choose sub-basin
-basin = "Mid-latitudinal Atlantic"
+sb = "Subtropical Atlantic"
 
-model = results[basin]
+model = results[sb]
 
 # get data for this basin
-group = merged[merged["sub_basin_name"] == basin].dropna(
+group = merged[merged["sub_basin_name"] == sb].dropna(
     subset=["origin_node_count"] + predictors
 ).copy()
 
-# save original shear for plotting
-rh600_original = group["rh600"].copy()
-
-# standardize predictors for model prediction
+# standardize predictors exactly as during model fitting
 scaler = StandardScaler()
 group[predictors] = scaler.fit_transform(group[predictors])
 
-# predictions
+# predict origin node counts using BOTH predictors
 group["Predicted"] = model.predict(group)
 
-# put back in time order
-group["rh600_original"] = rh600_original
+# put back in chronological order
 group = group.sort_values("year")
 
-# create figure
-fig, ax1 = plt.subplots(figsize=(14, 6))
+# plot
+fig, ax = plt.subplots(figsize=(14,6))
 
-# actual and predicted TC counts
-ax1.plot(
+# observed counts
+ax.plot(
     group["year"],
     group["origin_node_count"],
     color="black",
-    linewidth = 2,
-    label="Actual TC+TD Origin Nodes (count)",
-    zorder = 3
+    linewidth=2,
+    marker="o",
+    label="Observed TC+TD Origin Nodes"
 )
 
-ax1.plot(
+# predicted counts
+ax.plot(
     group["year"],
     group["Predicted"],
     color="tab:blue",
+    linewidth=2,
     linestyle="--",
-    linewidth = 2,
-    label="Predicted TC+TD Origin Nodes (count)",
-    zorder = 3
+    marker="s",
+    label="Predicted TC+TD Origin Nodes"
 )
 
-ax1.set_xlabel("Year")
-ax1.set_ylabel("TC+TD Origin Nodes", color="black")
+ax.set_xlabel("Year")
+ax.set_ylabel("TC+TD Origin Nodes")
 
-
-# secondary axis for shear
-ax2 = ax1.twinx()
-
-ax2.plot(
-    group["year"],
-    group["rh600_original"],
-    color="red",
-    linewidth=1,
-    alpha=0.5,
-    zorder=1,
-    label = "RH (%)"
+ax.set_title(
+    f"{sb}\nObserved vs. Predicted TC+TD Origin Nodes\n"
+    f"Multiple Linear Regression ($R^2$ = {model.rsquared:.2f})"
 )
 
-ax2.set_ylabel("RH (%)")
-
-
-# title with R2
-ax1.set_title(
-    f"{basin}\nActual vs. Predicted TC+TD Origin Locations and Relative Humidity (600hPa)\n$R^2$ = {model.rsquared:.2f}",
-    fontsize=12
-)
-
-# combine legends
-lines1, labels1 = ax1.get_legend_handles_labels()
-lines2, labels2 = ax2.get_legend_handles_labels()
-
-ax1.legend(
-    lines1 + lines2,
-    labels1 + labels2,
-    loc="center left",
-    bbox_to_anchor=(1.08, 0.5)
-)
+ax.legend()
 
 plt.tight_layout()
+plt.savefig(f"images/data_viz/MLR/TC+TD/gpi+sst_anom/actual_v_predicted_origins_gpi+sstAnom_{sb}.png")
+plt.show()
+
+######################################################################################################
+
+# # # actual v predicted for ONE variable
+# # choose sub-basin
+# basin = "Mid-latitudinal Atlantic"
+
+# model = results[basin]
+
+# # get data for this basin
+# group = merged[merged["sub_basin_name"] == basin].dropna(
+#     subset=["origin_node_count"] + predictors
+# ).copy()
+
+# # save original shear for plotting
+# rh600_original = group["rh600"].copy()
+
+# # standardize predictors for model prediction
+# scaler = StandardScaler()
+# group[predictors] = scaler.fit_transform(group[predictors])
+
+# # predictions
+# group["Predicted"] = model.predict(group)
+
+# # put back in time order
+# group["rh600_original"] = rh600_original
+# group = group.sort_values("year")
+
+# # create figure
+# fig, ax1 = plt.subplots(figsize=(14, 6))
+
+# # actual and predicted TC counts
+# ax1.plot(
+#     group["year"],
+#     group["origin_node_count"],
+#     color="black",
+#     linewidth = 2,
+#     label="Actual TC+TD Origin Nodes (count)",
+#     zorder = 3
+# )
+
+# ax1.plot(
+#     group["year"],
+#     group["Predicted"],
+#     color="tab:blue",
+#     linestyle="--",
+#     linewidth = 2,
+#     label="Predicted TC+TD Origin Nodes (count)",
+#     zorder = 3
+# )
+
+# ax1.set_xlabel("Year")
+# ax1.set_ylabel("TC+TD Origin Nodes", color="black")
+
+
+# # secondary axis for shear
+# ax2 = ax1.twinx()
+
+# ax2.plot(
+#     group["year"],
+#     group["rh600_original"],
+#     color="red",
+#     linewidth=1,
+#     alpha=0.5,
+#     zorder=1,
+#     label = "RH (%)"
+# )
+
+# ax2.set_ylabel("RH (%)")
+
+
+# # title with R2
+# ax1.set_title(
+#     f"{basin}\nActual vs. Predicted TC+TD Origin Locations and Relative Humidity (600hPa)\n$R^2$ = {model.rsquared:.2f}",
+#     fontsize=12
+# )
+
+# # combine legends
+# lines1, labels1 = ax1.get_legend_handles_labels()
+# lines2, labels2 = ax2.get_legend_handles_labels()
+
+# ax1.legend(
+#     lines1 + lines2,
+#     labels1 + labels2,
+#     loc="center left",
+#     bbox_to_anchor=(1.08, 0.5)
+# )
+
+# plt.tight_layout()
 # plt.savefig(f"images/data_viz/MLR/TC+TD/rh600/actual_vs_predicted_origin_nodes_rh600_{basin}.png")
 # plt.show()
 
@@ -347,8 +408,8 @@ plt.tight_layout()
 
 # print(vif_summary)
 
-# # save to csv
-# # vif_summary.to_csv("datasets/data_viz/MLR/VIF_shear_mslpAnom_sstMean_rh600.csv")
+# # # save to csv
+# # # vif_summary.to_csv("datasets/data_viz/MLR/VIF_shear_mslpAnom_sstMean_rh600.csv")
 
 ########################################################################################################################
 
